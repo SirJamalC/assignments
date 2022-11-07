0c0ca0953b23ac453a0f073fdbbecba0afcd1f4c
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-07 14:50:36 -0700

    updated

---
+++ Loop-Olympics/main.js
@@ -0,0 +1,10 @@
+//for loop that logs 0-9
+/*for(var i = 0; i < 10; i++){
+    console.log(i);
+}*/
+//for loop that logs 0-100 even numbers only
+for(var i = 0; i <= 100; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
