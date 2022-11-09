86154a9f0f36d1c4edc058fb63d2733ff7fdcf56
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-08 22:06:07 -0700

    completed first 4 steps

--- array.js
+++ array.js
@@ -2,7 +2,13 @@ var fruit = ["banana", "apple", "orange", "watermelon"];
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
 // Remove last item from vegetables array
 /*vegetables.pop();
-console.log(vegetables);*/
+console.log(vegetables);
 // Remove 1st item from fruit array
 fruit.shift();
+console.log(fruit);*/
+// Find the index of orange
+/*var orangeindex =fruit.indexOf("orange");
+console.log(orangeindex);*/
+// Add index of orange to end of fruit array
+fruit.push(2);
 console.log(fruit);
