5a76fbf666281f8264d836f7a380c07ca988b432
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-08 22:15:23 -0700

    completed first 6 steps

--- array.js
+++ array.js
@@ -10,5 +10,11 @@ console.log(fruit);*/
 /*var orangeindex =fruit.indexOf("orange");
 console.log(orangeindex);*/
 // Add index of orange to end of fruit array
-fruit.push(2);
-console.log(fruit);
+/*fruit.push(2);
+console.log(fruit);*/
+// Use length property to find length of vegetables array
+/*let veglength = vegetables.length;
+console.log(veglength);*/
+// Add length of veg array to the end of vegtables array
+vegetables.push(4);
+console.log(vegetables);
