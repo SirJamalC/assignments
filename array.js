358093d0825dd46a0eb9c96667f0c4d910aecb00
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-09 10:43:00 -0700

    finished

--- array.js
+++ array.js
@@ -1,27 +1,32 @@
 var fruit = ["banana", "apple", "orange", "watermelon"];
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
 // Remove last item from vegetables array
-/*vegetables.pop();
-console.log(vegetables);
+vegetables.pop();
+//console.log("vegetables:", vegetables);
 // Remove 1st item from fruit array
 fruit.shift();
-console.log(fruit);*/
+//console.log("fruit;", fruit);
 // Find the index of orange
-/*var orangeindex =fruit.indexOf("orange");
-console.log(orangeindex);*/
+var orangeindex =fruit.indexOf("orange");
+//console.log(orangeindex);
 // Add index of orange to end of fruit array
-/*fruit.push(2);
-console.log(fruit);*/
+fruit.push(orangeindex);
+//console.log("fruit:", fruit);
 // Use length property to find length of vegetables array
-/*let veglength = vegetables.length;
-console.log(veglength);*/
+let veglength = vegetables.length;
+//console.log(veglength);
 // Add length of veg array to the end of vegtables array
-/*vegetables.push(4);
-console.log(vegetables);*/
+vegetables.push(veglength);
+//console.log("vegetables:", vegetables);
 // Place both array into one and name it food 
 // Remove 2 elements from array starting @ index 4
 food = fruit.concat(vegetables);
-var foodArray = food.slice(4, 2);
-console.log(foodArray);
-
+//console.log("food:", food);
+var foodArray = food.splice(4, 2);
+//console.log("food:", food);
+var reveresed = food.reverse();
+//console.log(reveresed);
+// Join array into string
+joined = food.join(",")
+console.log(joined);
 
