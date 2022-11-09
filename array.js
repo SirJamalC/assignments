9a874a9d56871077a0b7a08d842428fbf26079b3
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-08 21:58:44 -0700

    completed 1st 2 steps

---
+++ array.js
@@ -0,0 +1,8 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+// Remove last item from vegetables array
+/*vegetables.pop();
+console.log(vegetables);*/
+// Remove 1st item from fruit array
+fruit.shift();
+console.log(fruit);
