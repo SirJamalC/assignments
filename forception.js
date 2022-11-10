b11f13c68bc7c60c63da3a7a76d78823673bcf5a
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-10 09:14:19 -0700

    add

--- forception.js
+++ forception.js
@@ -1,2 +1,9 @@
+function forception(people, alphabet){
+    for (var i = 0; i < people.length; i++){
+
+    }
+}
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
 var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var splitAl = alphabet.split("")
+console.log(splitAl);
