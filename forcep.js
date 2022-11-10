47a52300e28741aede83c0140b42dda6abfc5f7d
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-10 09:30:24 -0700

    working

---
+++ forcep.js
@@ -0,0 +1,11 @@
+// Write a function that accepts 2 arrays as paramaters
+// Use a for loop within a for loop to create a new array
+function forception(people, alphabet){
+    for(var i = 0; i < people.length; i++){
+
+    }
+// created variables for array 1 and used split method to create 2nd array from alphabet string
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var alpArray = alphabet.split("");
+console.log(alpArray);
