932d776f80839889202f3456d355d5c9387c70f4
Jamal Campbell <sirjamalcampbell24@gmail.com>
2022-11-17 10:16:47 -0700

    WIP

--- Business-Time-Proj/main.js
+++ Business-Time-Proj/main.js
@@ -1,11 +1,18 @@
 // create variable for button for eventListner
 const btn = document.getElementById("btn");
 // add eventListner to button
-btn.addEventListener("click", function handleClick(event){
-    event.preventDefault()
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+btn.addEventListener("submit", function (e){
+    e.preventDefault()
     // create variable for input
     const form = document.getElementById("form");
-    console.log(form.value)
-    // clear input filed
-    form.value = "";
+    console.log(fname.value, lname.value, email.value);
+    clearInputs();
 });
+function clearInputs(){
+    fname.value = "";
+    lname.value = "";
+    email.value = "";
+};
