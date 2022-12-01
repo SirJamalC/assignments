8f9c6cbf1580ddc0e59fedb94eb17b5a5dc4f1d6
Jamal_Campbell <sirjamalcampbell24@gmail.com>
2022-12-01 10:47:22 -0700

    WIP

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -1,5 +1,5 @@
-var form = document.getElementByID("airline-form");
-var submit = document.getElementByID("submit");
+var form = document.getElementByID("arline-form");
+var submit = document.getElementByID(submit);
 
 function formAlert() {
     var firstName = form.elements["firstName"].value;
@@ -8,16 +8,20 @@ function formAlert() {
     var gender = form.elements["gender"].value;
     var location = form.elements["travel-location"].value;
     var diet = {};
-    if (form.elements["vegan"].checked) {
+    if (form.elements['vegan'].checked) {
         diet.push(document.getElementById("vegan").value);
-    }else if(form.elements["gluten"].checked) {
-        diet.push(document.getElementById("gluten").value);
-    
-    }else{ (form.elements["paleo"].checked) 
-        diet.push(document.getElementById("paleo").value);
-    }
-    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
 
+    } else if (form.elements['gluten'].checked) {
+        diet.push(document.getElementById('gluten').value);
+    
+    } else { (form.elements['paleo'].checked) 
+        diet.push(document.getElementById('paleo').value);
+    
+    }
+
+
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
 }
+
 
 submit.addEventListener("click", formAlert);
